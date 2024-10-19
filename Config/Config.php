e2633c663978a26d6852c5d886566a8f2aaab062
JrnWilliam <antoni.will91@gmail.com>
2024-10-18 23:17:25 -0600

    Se creo el archivo con los parametros de la configuracion para la conexion a la base de datos

---
+++ Config/Config.php
@@ -0,0 +1,10 @@
+<?php
+  const BASE_URL = "http://localhost/sistema_credito/";
+
+  const CONEXION = false;
+  const DB_HOST = "localhost";
+  const DB_NAME= "sistemacredito";
+  const DB_USER = "root";
+  const DB_PASSWORD = "";
+  const DB_CHARSET = "utf8";
+?>
