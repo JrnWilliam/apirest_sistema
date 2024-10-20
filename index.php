4d7d4bde38492449b12c1cb6dbce51c0fb36a573
JrnWilliam <antoni.will91@gmail.com>
2024-10-19 18:41:57 -0600

    Este codigo procesa la url para determinar que controlador, metodo o parametro se debe cargar en la aplicación cargando llos recursos necesarios

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+    require_once("Config/Config.php");
+    require_once("Helpers/Helpers.php");
+    $url = !empty($_GET['url']) ? $_GET['url'] : "home/home";
+
+    $arrayurl = explode("/",$url);
+    $controller= $arrayurl[0];
+    $method = $arrayurl[0];
+    $params = "";
+
+    if (!empty($arrayurl[1]))
+    {
+        if($arrayurl[1] != "")
+        {
+            $method = $arrayurl[1];
+        }
+    }
+
+    if(!empty($arrayurl[2]))
+    {
+        if($arrayurl[2] != "")
+        {
+            for ($i = 2; $i < count($arrayurl); $i++)
+            { 
+                $params .= $arrayurl[$i] . ', ';
+            }
+            $params = trim($params,",");
+        }
+    }
+
+    require_once("Libraries/Core/Autoload.php");
+    require_once("Libraries/Core/Load.php");
+?>
