8f52530a2c7f27c4c59ddc20a1131c0a1ad24c5a
JrnWilliam <antoni.will91@gmail.com>
2024-10-21 20:22:28 -0600

    Se creo el codigo necesario para manejar las operaciones referente a los clientes

--- Controllers/Cliente.php
+++ Controllers/Cliente.php
@@ -1,3 +1,34 @@
 <?php
-    
+    class Cliente extends Controllers
+    {
+        public function __construct()
+        {
+            parent::__construct();
+        }
+
+        public function Seleccionarcliente($idcliente)
+        {
+            echo "Controlador Cliente" . $idcliente;
+        }
+
+        public function RegistroClientes()
+        {
+            echo "Registro de Clientes";
+        }
+
+        public function MostrarClientes()
+        {
+            echo "Registro de Clientes";
+        }
+
+        public function EliminarCliente($idcliente)
+        {
+            echo "Registro de Clientes" . $idcliente;
+        }
+
+        public function ActualizarCliente($idcliente)
+        {
+            echo "Registro de Clientes" . $idcliente;
+        }
+    }
 ?>
