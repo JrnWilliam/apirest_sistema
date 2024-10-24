c2335cbd6244687c7dce0453431e43765e186cef
JrnWilliam <antoni.will91@gmail.com>
2024-10-23 21:58:18 -0600

    Se creo el codigo para validar los datos del cliente, tanto con funciones que manipulan expresiones regulares como con funciones que evitan la inyeccion SQL

--- Controllers/Cliente.php
+++ Controllers/Cliente.php
@@ -20,8 +20,58 @@
 
                 if($metodo == "POST")
                 {
-                    $respuesta = array('status' => true, 'msg' => 'Datos Guardados Correctamente' );
-                    $codigo = 200;
+                    $_POST = json_decode(file_get_contents('php://input'),true);
+
+                    if(empty($_POST['identificacion']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'La Identificacion es Requerida' );
+                        JSONRespuesta($respuesta,200);
+                        die();
+                    }
+
+                    if(!ValidarNombre($_POST['nombres']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'El Nombre no Contiene un Formato Correcto');
+                        JSONRespuesta($respuesta,200);
+                        die();
+                    }
+
+                    if(!ValidarNombre($_POST['apellidos']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'El Apellido no Contiene un Formato Correcto');
+                        JSONRespuesta($respuesta,200);
+                        die();
+                    }
+
+                    if(!ValidarNumero($_POST['telefono']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'El Formato del Numero es Invalido' );
+                        JSONRespuesta($respuesta,200);
+                        die();
+                    }
+
+                    if(!ValidarCorreo($_POST['email']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'El Formato del Correo es Invalido' );
+                        JSONRespuesta($respuesta,200);
+                        die();
+                    }
+
+                    if(empty($_POST['direccion']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'La Direccion es Requerida' );
+                        JSONRespuesta($respuesta,200);
+                        die();
+                    }
+                    $identificacion = $_POST['identificacion'];
+                    $nombres = $_POST['nombres'];
+                    $apellidos = $_POST['apellidos'];
+                    $telefono = $_POST['telefono'];
+                    $correo = $_POST['email'];
+                    $direccion = $_POST['direccion'];
+                    $nit = !empty($_POST['nit']) ? LimpiarCadena($_POST['nit']) : "";
+                    $nfiscal = !empty($_POST['nombrefiscal']) ? LimpiarCadena($_POST['nombrefiscal']) : "";
+                    $dirfiscal = !empty($_POST['direccionfiscal']) ? LimpiarCadena($_POST['direccionfiscal']) : "";
                 }
                 else
                 {
