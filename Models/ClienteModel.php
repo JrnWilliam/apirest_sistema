98288e25a6c726d5d13be2ee9bc43de028465b60
JrnWilliam <antoni.will91@gmail.com>
2024-10-24 20:46:49 -0600

    Se crearon las propiedades que contendran los datos del cliente asi como la funion encargada de setear dichos datos

--- Models/ClienteModel.php
+++ Models/ClienteModel.php
@@ -1,10 +1,34 @@
 <?php
   class ClienteModel extends MySQL
   {
+    private $intIdCliente;
+    private $strIdentificación;
+    private $strNombres;
+    private $strApellidos;
+    private $intTelefono;
+    private $strEmail;
+    private $strDireccion;
+    private $strNit;
+    private $strNomFiscal;
+    private $strDirFiscal;
+    private $intStatus;
+
     public function __construct()
     {
       parent::__construct();   
     }
 
-  }  
+    public function AgregarCliente(string $identificacion,string $nombres, string $apellidos, int $telefono, string $email, string $direccion, string $nit, string $nombrefiscal, string $dirfiscal)
+    {
+        $this->strIdentificación = $identificacion;
+        $this->strNombres = $nombres;
+        $this->strApellidos = $apellidos;
+        $this->intTelefono = $telefono;
+        $this->strEmail = $email;
+        $this->strDireccion  = $direccion;
+        $this->strNit = $nit;
+        $this->strNomFiscal = $nombrefiscal;
+        $this->strDirFiscal = $dirfiscal;
+    }
+  }
 ?>
