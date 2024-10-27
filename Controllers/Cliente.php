9b1e2687833908060b7ed639f04965ba14befe19
JrnWilliam <antoni.will91@gmail.com>
2024-10-26 22:49:50 -0600

    Se agrego el codigo para validacion de datos

--- Controllers/Cliente.php
+++ Controllers/Cliente.php
@@ -25,42 +25,42 @@
                     if(empty($_POST['identificacion']))
                     {
                         $respuesta = array('status' => false, 'msg' => 'La Identificacion es Requerida' );
-                        JSONRespuesta($respuesta,405);
+                        JSONRespuesta($respuesta,400);
                         die();
                     }
 
                     if(!ValidarNombre($_POST['nombres']))
                     {
                         $respuesta = array('status' => false, 'msg' => 'El Nombre no Contiene un Formato Correcto');
-                        JSONRespuesta($respuesta,405);
+                        JSONRespuesta($respuesta,400);
                         die();
                     }
 
                     if(!ValidarNombre($_POST['apellidos']))
                     {
                         $respuesta = array('status' => false, 'msg' => 'El Apellido no Contiene un Formato Correcto');
-                        JSONRespuesta($respuesta,405);
+                        JSONRespuesta($respuesta,400);
                         die();
                     }
 
                     if(!ValidarNumero($_POST['telefono']))
                     {
                         $respuesta = array('status' => false, 'msg' => 'El Formato del Numero es Invalido' );
-                        JSONRespuesta($respuesta,405);
+                        JSONRespuesta($respuesta,400);
                         die();
                     }
 
                     if(!ValidarCorreo($_POST['email']))
                     {
                         $respuesta = array('status' => false, 'msg' => 'El Formato del Correo es Invalido' );
-                        JSONRespuesta($respuesta,405);
+                        JSONRespuesta($respuesta,400);
                         die();
                     }
 
                     if(empty($_POST['direccion']))
                     {
                         $respuesta = array('status' => false, 'msg' => 'La Direccion es Requerida' );
-                        JSONRespuesta($respuesta,405);
+                        JSONRespuesta($respuesta,400);
                         die();
                     }
                     $identificacion = $_POST['identificacion'];
@@ -85,7 +85,7 @@
                     else
                     {
                         $respuesta = array('status' => false, 'msg' => 'Identificación o el Correo ya Existen', $metodo);
-                        $codigo = 405;
+                        $codigo = 400;
                     }
                 }
                 else
@@ -122,6 +122,68 @@
 
                 if($metodo == "PUT")
                 {
+                    $parametros = json_decode(file_get_contents('php://input'),true);
+
+                    if(empty($idcliente) or !is_numeric($idcliente))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'Error en los Parametros');
+                        $codigo = 400;
+
+                        JSONRespuesta($respuesta,$codigo);
+                        die();
+                    }
+                    if(empty($parametros['identificacion']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'La Identificacion es Requerida' );
+                        JSONRespuesta($respuesta,400);
+                        die();
+                    }
+
+                    if(empty($parametros['nombres']) || !ValidarNombre($parametros['nombres']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'El Nombre no Contiene un Formato Correcto');
+                        JSONRespuesta($respuesta,400);
+                        die();
+                    }
+
+                    if(empty($parametros['apellidos']) or !ValidarNombre($parametros['apellidos']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'El Apellido no Contiene un Formato Correcto');
+                        JSONRespuesta($respuesta,400);
+                        die();
+                    }
+
+                    if(empty($parametros['telefono']) || !ValidarNumero($parametros['telefono']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'El Formato del Numero es Invalido' );
+                        JSONRespuesta($respuesta,400);
+                        die();
+                    }
+
+                    if(!ValidarCorreo($parametros['email']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'El Formato del Correo es Invalido' );
+                        JSONRespuesta($respuesta,400);
+                        die();
+                    }
+
+                    if(empty($parametros['direccion']))
+                    {
+                        $respuesta = array('status' => false, 'msg' => 'La Direccion es Requerida' );
+                        JSONRespuesta($respuesta,400);
+                        die();
+                    }
+
+                    $identificacion = $parametros['identificacion'];
+                    $nombres = ucwords(strtolower($parametros['nombres']));
+                    $apellidos = ucwords(strtolower($parametros['apellidos']));
+                    $telefono = $parametros['telefono'];
+                    $correo = strtolower($parametros['email']);
+                    $direccion = $parametros['direccion'];
+                    $nit = !empty($parametros['nit']) ? LimpiarCadena($parametros['nit']) : "";
+                    $nfiscal = !empty($parametros['nombrefiscal']) ? LimpiarCadena($parametros['nombrefiscal']) : "";
+                    $dirfiscal = !empty($parametros['direccionfiscal']) ? LimpiarCadena($parametros['direccionfiscal']) : "";
+
                     $respuesta = array('status' => true, 'msg' => 'Datos Actualizados Correctamente', 'data' => "");
                     $codigo = 200;
                 }
