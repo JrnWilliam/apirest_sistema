f737ceabcf71d459332ef0b146bbfac7099900b0
JrnWilliam <antoni.will91@gmail.com>
2024-10-29 16:29:27 -0600

    Se corrigio el codigo

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
             { 
                 $params .= $arrayurl[$i] . ', ';
             }
-            $params = trim($params,",");
+            $params = trim($params,", ");
         }
     }
 
