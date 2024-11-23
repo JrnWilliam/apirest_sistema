98618b44f546f8cdffbe296a6ee523cdfdd6ae39
JrnWilliam <antoni.will91@gmail.com>
2024-11-23 13:31:19 -0600

    Se valida que se realice via solicitud POST, se validad los datos que se registraran y se recepcionan los datos almacenandolos en variables

--- Controllers/Movimiento.php
+++ Controllers/Movimiento.php
@@ -6,6 +6,58 @@
         parent::__construct();        
     }
 
-    
+    public function RegistrarTipoMovimiento()
+    {
+        try
+        {
+            $metodo = $_SERVER['REQUEST_METHOD'];
+            $respuesta = [];
+
+            if($metodo == "POST")
+            {
+                $_POST = json_decode(file_get_contents('php://input'),true);
+                if(empty($_POST['movimiento']))
+                {
+                    $respuesta = array('status' => false, 'msg' => 'Ingrese el Movimiento');
+                    JSONRespuesta($respuesta,406);
+                    die();
+                }
+                if(empty($_POST['tipomovimiento']) or ($_POST['tipomovimiento'] != 1 AND $_POST['tipomovimiento'] != 2))
+                {
+                    $respuesta = array('status' => false, 'msg' => 'Error en el Tipo de Movimiento');
+                    JSONRespuesta($respuesta,406);
+                    die();
+                }
+                if(empty($_POST['descripcion']))
+                {
+                    $respuesta = array('status' => false, 'msg' => 'Ingrese una Descripción');
+                    JSONRespuesta($respuesta,406);
+                    die();
+                }
+                $movimiento = ucwords(LimpiarCadena($_POST['movimiento']));
+                $tipomovimiento = ucwords(LimpiarCadena($_POST['tipomovimiento']));
+                $descripcion = ucwords(LimpiarCadena($_POST['descripcion']));
+                
+                $codigo = 200;
+            }
+            else
+            {
+                $respuesta = array('status' => false, 'msg' => 'Error en la Solicitud ' . $metodo);
+                $codigo = 405;
+            }
+            JSONRespuesta($respuesta,$codigo);
+            die();
+        }
+        catch(Exception $e)
+        {
+            echo "Error en el Proceso " . $e->getMessage();
+        }
+        die();
+    }
+
+    public function TiposMovimiento()
+    {
+        echo "Mostrar los Tipos de Movimiento";
+    }
   }  
 ?>
