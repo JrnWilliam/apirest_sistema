399e7ef50a3a93d610b9bbeca9872e427407a5f5
JrnWilliam <antoni.will91@gmail.com>
2024-11-25 19:56:34 -0600

    De no obtener un registro al validar se inserta el nuevo registro

--- Models/MovimientoModel.php
+++ Models/MovimientoModel.php
@@ -19,6 +19,17 @@
             $sql = "SELECT *FROM tipo_movimiento WHERE movimiento = : mov AND estatus = :sts";
             $parametro = array(":mov" => $this->strMovimiento, ":sts" => 1);
             $solicitud = $this->SeleccionarUnRegistro($sql,$parametro);
+            if(empty($solicitud))
+            {
+                $consulta = "INSERT INTO tipo_movimiento(movimiento,tipomovimiento,descripcion) VALUES (:mov, :tipomov, :descr)";
+                $param = array(":mov" => $this->strMovimiento,":tipomov" => $this->intTipoMovimiento,":descr" => $this->strDescripcion);
+                $solicitudinsert = $this->InsertarRegistro($consulta,$param);
+                return $solicitudinsert;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 ?>
