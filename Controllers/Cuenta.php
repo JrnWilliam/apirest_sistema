fce4a081b0c986d016c0520e8447e53e1b9eabed
JrnWilliam <antoni.will91@gmail.com>
2024-11-27 23:47:52 -0600

    Se valida que la solicitud HTTP sea post para proceder a enviar los registros

--- Controllers/Cuenta.php
+++ Controllers/Cuenta.php
@@ -18,7 +18,32 @@
 
         public function RegistrarCuenta()
         {
-            echo "Cuenta Registrada Correctamente";
+            try
+            {
+                $metodo = $_SERVER['REQUEST_METHOD'];
+                $respuesta = [];
+
+                if($metodo=="POST")
+                {
+                    $_POST = json_decode(file_get_contents('php://input'),true);
+
+                    if(empty($_POST['']))
+                    echo "Registro Exitoso";
+                    $codigo = 200;
+                }
+                else
+                {
+                    $respuesta = array('status' =>false, 'msg' => 'Error en la Solicitud ' . $metodo);
+                    $codigo = 400;
+                }
+                JSONRespuesta($respuesta,$codigo);
+                die();
+            }
+            catch(Exception $e)
+            {
+                echo "Error en el Proceso " .$e->getMessage();
+            }
+            die();
         }
     }
 ?>
