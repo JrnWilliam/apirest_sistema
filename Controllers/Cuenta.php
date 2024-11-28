a5169cc664942d28d25103ce983ae4110be45443
JrnWilliam <antoni.will91@gmail.com>
2024-11-27 20:44:12 -0600

    Se configuro el controlador cliente con los metodos para gestionar las solicitudes que se haran al controlador de la misma clase

--- Controllers/Cuenta.php
+++ Controllers/Cuenta.php
@@ -1,3 +1,24 @@
 <?php
-    
+    class Cuenta extends Controllers
+    {
+        public function __construct()
+        {
+            parent::__construct();
+        }
+
+        public function SeleccionarCuenta($idcuenta)
+        {
+            echo "Cuenta {$idcuenta} Seleccionada Correctamente";
+        }
+
+        public function MostrarCuentas()
+        {
+            echo "Mostrar Todas las Cuentas";
+        }
+
+        public function RegistrarCuenta()
+        {
+            echo "Cuenta Registrada Correctamente";
+        }
+    }
 ?>
