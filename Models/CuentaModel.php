792a4c71addb223a490529cec5bd679e3a2dcafc
JrnWilliam <antoni.will91@gmail.com>
2024-11-28 23:18:14 -0600

    Se crearon las propiedades y el metodo setCuenta que registrara los datos de la cuenta

--- Models/CuentaModel.php
+++ Models/CuentaModel.php
@@ -1,9 +1,31 @@
 <?php
-    class Cuenta extends MYSQL
+    class CuentaModel extends MYSQL
     {
+        private $intClienteId;
+        private $intProductoId;
+        private $intFrecuenciaId;
+        private $fltMonto;
+        private $intCuotas;
+        private $fltMontoCuotas;
+        private $fltCargo;
+        private $fltSaldo;
+
         public function __construct()
         {
             parent::__construct();
+        }
+
+        public function setCuenta(int $clienteid, int $productoid, int $frecuenciaid, float $monto, int $cuotas, float $montocuotas, float $cargo, float $saldo)
+        {
+            $this->intClienteId = $clienteid;
+            $this->intProductoId = $productoid;
+            $this->intFrecuenciaId = $frecuenciaid;
+            $this->fltMonto = $monto;
+            $this->intCuotas = $cuotas;
+            $this->fltCargo = $cargo;
+            $this->fltSaldo = $saldo;
+
+            $sql ="";
         }
     }
 ?>
